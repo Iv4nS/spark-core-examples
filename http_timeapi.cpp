93ad74e6457651596a9ff220785dd2128e4647b3
Synox <synox@synox>
2014-01-11 19:01:24 +0100

    doc

--- http_timeapi.cpp
+++ http_timeapi.cpp
@@ -25,13 +25,103 @@ void loop() {
 	delay(500);               // Wait for 1 second
 
 	long timestamp = currentTimeMilis();
-	Serial.begin(9600);
+	Serial.print("time: ");
+	Serial.println(timestamp);
 
 	nextTime = millis() + 10000;
 }
 
+
+
+// ------------- HTTP functions --------------
+
+/**
+ * make http request and return body
+ */
 TCPClient client;
 char buffer[1024];
+String http_get(char const* hostname, String path) {
+
+	if (client.connect(hostname, 80)) {
+		client.print("GET ");
+		client.print(path);
+		client.print(" HTTP/1.0\n");
+		client.print("HOST: ");
+		client.println(hostname);
+		client.print("\n");
+		//	client.print("Connection: close\n\n");
+		client.flush();
+	} else {
+		Serial.println("connection failed");
+		client.stop();
+		return NULL;
+	}
+
+	// Block until first byte is read.
+	client.read();
+	for (unsigned int i = 0; i < sizeof(buffer) && client.available(); i++) {
+		char c = client.read();
+		if (c == -1) {
+			break;
+		}
+		buffer[i] = c;
+	}
+	client.stop();
+
+	String response(buffer);
+	int bodyPos = response.indexOf("\r\n\r\n");
+	if (bodyPos == -1) {
+		Serial.println("can not find http reponse body");
+				return NULL;
+	}
+	return response.substring(bodyPos+4);
+}
+
+
+
+// ------------- DATE / TIME functions --------------
+/**
+ * parse a string of the form "2014-01-11T17:17:59+0200"
+ */
+long parseDate(String str) {
+	// make sure it works with UTC
+	setenv("TZ", "UTC", 1);
+	tzset();  // recognize TZ
+	// sy/11.Jan.2014: Not sure if setenv works at all...
+
+	// parse date
+	struct tm time;
+	strptime(str.c_str(), "%Y-%m-%dT%H:%M:%S", &time);
+	return (long) mktime(&time); //timegm should be used, but is not available
+}
+
+/**
+ * can parse the timezone offset in the string "2014-01-11T17:17:59+0100"
+ */
+long parseTzOffset(String str) {
+	// strptime currently does not parse the timezone with %z, so we do it ourself:
+	// parse 3 digits the "+0100" which result in 1 hour.
+	int offsetHours;
+	sscanf(str.c_str(), "%*19s%3d", &offsetHours);
+	return offsetHours * 3600;
+}
+
+/**
+ * returns current time in milisceconds, from a http server.
+ */
+long currentTimeMilis() {
+	Serial.println("getting current time");
+
+	String response = http_get("www.timeapi.org", "/utc/now");
+	if (response != NULL) {
+		Serial.print("time=");
+		Serial.println(response);
+		return parseDate(response);
+	} else {
+		return 0;
+	}
+}
+
 
 /*timeapi example :
  *
@@ -50,82 +140,3 @@ char buffer[1024];
 
  2014-01-11T11:59:37+00:00
  */
-
-String http_get(char const* hostname, String path) {
-
-	if (client.connect(hostname, 80)) {
-		if (DEBUG)
-			Serial.println("connected");
-		client.print("GET ");
-		client.print(path);
-		client.print(" HTTP/1.0\n");
-		client.print("HOST: ");
-		client.println(hostname);
-		client.print("\n");
-		//	client.print("Connection: close\n\n");
-		client.flush();
-	} else {
-		Serial.println("connection failed");
-		client.stop();
-		return NULL;
-	}
-
-	// Block until first byte is read.
-	client.read();
-
-	if (DEBUG)
-		Serial.println("reading response....");
-	for (unsigned int i = 0; i < sizeof(buffer) && client.available(); i++) {
-		char c = client.read();
-		if (c == -1) {
-			break;
-		}
-		buffer[i] = c;
-	}
-
-	client.stop();
-
-	String response(buffer);
-	int bodyPos = response.indexOf("\r\n\r\n");
-	if (bodyPos != -1) {
-		Serial.println("found http body:");
-		bodyPos += 4; // skip RNRN
-	} else {
-		Serial.println("can not find http reponse body");
-		return NULL;
-	}
-	response = response.substring(bodyPos);
-	return response;
-}
-
-long parseDate(String str) {
-	// make sure it works on UTC
-	setenv("TZ", "UTC", 1);
-	tzset();                // recognize TZ
-
-	// parse date
-	struct tm t1;
-	strptime(str.c_str(), "%Y-%m-%dT%H:%M:%S", &t1);
-	return (long) mktime(&t1);
-}
-
-long parseTzOffset(String str) {
-	// strptime currently does not parse the timezone with %z, so we do it ourself:
-	// parse 3 digits the "+0100" which result in 1 hour.
-	int offsetHours;
-	sscanf(str.c_str(), "%*19s%3d", &offsetHours);
-	return offsetHours * 3600;
-}
-
-long currentTimeMilis() {
-	Serial.println("getting current time");
-
-	String response = http_get("www.timeapi.org", "/utc/now");
-	if (response != NULL) {
-		Serial.print("time=");
-		Serial.println(response);
-		return parseDate(response);
-	} else {
-		return 0;
-	}
-}
